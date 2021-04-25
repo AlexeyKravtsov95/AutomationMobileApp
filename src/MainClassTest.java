0950582db89ce4e94087c0cbcb654504dfeb902f
kravtsov <al.kravtsov@cft.ru>
2021-04-25 14:33:26 +0700

    Test 2
    Ответ к заданию Тест 2 + поменял название теста по Тесту 1

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,12 +4,20 @@ import org.junit.Test;
 public class MainClassTest
 {
 
+    MainClass main = new MainClass();
+
     @Test
-    public void firstTest()
+    public void testGetLocalNumber()
     {
-        MainClass main = new MainClass();
         int actual = main.getLocalNumber(14);
         int expected = 14;
-        Assert.assertTrue("Метод getLocalNumber возращает неожидаемый резульат", actual == expected);
+        Assert.assertTrue("Метод getLocalNumber возращает неожидаемый результат", actual == expected);
+    }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        int actual = main.getClassNumber();
+        Assert.assertTrue("Метод getClassNumber возращает число меньше 45", actual > 45);
     }
 }
